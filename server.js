4dbc6587700aedd6b9b87e5fea43e3e584b08932
unknown <Nayan.Prakash@LARENON.NET>
2022-07-28 10:43:20 +0530

    dist

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
+// Get dependencies
 const express = require('express');
+const app = express();
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-const app = express();
+require('dotenv').config();
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-extended: false
+	extended: false
 }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, '/dist/architectui-angular-free/')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, '/dist/architectui-angular-free/'));
+	res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-// Get port from environment and store in Express.
-let port = process.env.PORT || '7872';
+/*  Get port from environment and store in Express. */
+const port = process.env.PORT || '7872';
 app.set('port', port);
 
-// Create HTTP server.
+/*  Create HTTP server. */
 const server = http.createServer(app);
 
-// Listen on provided port, on all network interfaces.
-server.listen(port, () => console.log(`APP is running on localhost: ${port}`))
+/*  Listen on provided port, on all network interfaces. */
+server.listen(port, () => console.log(`Parking lot project is running on port: ${port}`));
